525e8dcb5d099f95a7ebb4c6fed0696f6bd5f0f8
qwer0114 <skyok203@naver.com>
2024-07-03 23:46:31 +0900

    feat: globalStyle 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,31 +3,31 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
-    a{
+    a {
         text-decoration: none;
         color: inherit;
     }
-    *{
+    * {
         box-sizing: border-box;
     }
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
-    a, dl, dt, dd, ol, ul, li, form, label, table{
+    a, dl, dt, dd, ol, ul, li, form, label, table {
         margin: 0;
         padding: 0;
         border: 0;
         font-size: 10px;
         vertical-align: baseline;
     }
-    body{
+    body {
         line-height: 1;
         font-family: 'Noto Sans KR', sans-serif;
-        background-color: #F6F9F0
-         /* 파이어폭스 */
+        background-color: #F6F9F0;
+        /* 파이어폭스 */
         scrollbar-width: none;
         /* IE 브라우저 */
         -ms-overflow-style: none;
     }
-    ol, ul{
+    ol, ul {
         list-style: none;
     }
     button {
@@ -35,8 +35,8 @@ const GlobalStyles = createGlobalStyle`
         background: transparent;
         cursor: pointer;
     }
-    body::webkit-scrollbar {
-        * 크롬 사파리 etc */
+    body::-webkit-scrollbar {
+        /* 크롬 사파리 etc */
         display: none;
     }
 `;
