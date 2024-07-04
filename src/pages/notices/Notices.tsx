e0a7001828ffbd4bfe5770b5633d49d7dfdbc0a3
qwer0114 <skyok203@naver.com>
2024-07-04 21:50:49 +0900

    feat: Notice 페이지 정의

--- src/pages/notices/Notices.tsx
+++ src/pages/notices/Notices.tsx
@@ -1,5 +1,71 @@
+import styled from "styled-components";
+import Article from "./components/Article";
+import SearchBar from "./components/SearchBar";
+import Menu from "./components/Menu";
+import { useEffect, useState } from "react";
+
 function Notices() {
-  return <div>notices</div>;
+  const [menu, setMenu] = useState<string>("일반");
+  const NOTICES = [
+    {
+      title:
+        "[자연진로취업지원팀] 2024-1학기 온라인 모의 면접프로그램 참여 학생 모집",
+      link: "https://www.mju.ac.kr/bbs/mjukr/141/213267/artclView.do",
+      date: "07.04",
+      id: 1,
+    },
+    {
+      title:
+        "[자연진로취업지원팀] 2024-1학기 온라인 모의 면접프로그램 참여 학생 모집",
+      link: "https://www.mju.ac.kr/bbs/mjukr/141/213267/artclView.do",
+      date: "07.04",
+      id: 2,
+    },
+    {
+      title:
+        "[자연진로취업지원팀] 2024-1학기 온라인 모의 면접프로그램 참여 학생 모집",
+      link: "https://www.mju.ac.kr/bbs/mjukr/141/213267/artclView.do",
+      date: "07.04",
+      id: 3,
+    },
+    {
+      title:
+        "[자연진로취업지원팀] 2024-1학기 온라인 모의 면접프로그램 참여 학생 모집",
+      link: "https://www.mju.ac.kr/bbs/mjukr/141/213267/artclView.do",
+      date: "07.04",
+      id: 4,
+    },
+    {
+      title:
+        "[자연진로취업지원팀] 2024-1학기 온라인 모의 면접프로그램 참여 학생 모집",
+      link: "https://www.mju.ac.kr/bbs/mjukr/141/213267/artclView.do",
+      date: "07.04",
+      id: 5,
+    },
+  ];
+  useEffect(() => {}, [menu]);
+  return (
+    <>
+      <SearchBar />
+      <Menu menu={menu} setMenu={setMenu} />
+      <NoticeWrapper>
+        {NOTICES.map((notice) => (
+          <Article
+            title={notice.title}
+            date={notice.date}
+            link={notice.link}
+            key={notice.id}
+          />
+        ))}
+      </NoticeWrapper>
+    </>
+  );
 }
 
 export default Notices;
+const NoticeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+`;
