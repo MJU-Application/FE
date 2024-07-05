fa1430d043eb353e0af5e3f93e54cc3d2bf47a63
qwer0114 <skyok203@naver.com>
2024-07-05 23:46:12 +0900

    feat: 공지 메뉴 정의

--- src/pages/notices/components/Menu.tsx
+++ src/pages/notices/components/Menu.tsx
@@ -1,3 +1,5 @@
+import { NOTICE } from "@/constants/notice";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 type MenuProps = {
@@ -6,37 +8,62 @@ type MenuProps = {
 };
 
 function Menu({ menu, setMenu }: MenuProps) {
-  const MENU = ["일반", "학사", "장학/학자금", "취창업"];
-
+  const navigate = useNavigate();
   return (
     <MenuBar>
-      {MENU.map((item) => (
+      {NOTICE.map((notice) => (
         <div
-          className={`menu ${menu === item ? "activate" : "inactivate"}`}
-          key={item}
-          onClick={() => setMenu(item)}
+          className={`menu ${menu === notice.name ? "activate" : "inactivate"}`}
+          key={notice.name}
+          onClick={() => {
+            setMenu(notice.name);
+            navigate(`/notice${notice.query}`);
+          }}
         >
-          {item}
+          {notice.name}
         </div>
       ))}
     </MenuBar>
   );
 }
+
 export default Menu;
 
 const MenuBar = styled.div`
   display: flex;
   justify-content: space-around;
   margin-top: 2rem;
+
   .menu {
     cursor: pointer;
-    font-size: 2rem;
+    font-size: 1.8rem;
     font-weight: 600;
     color: #838496;
     padding-bottom: 0.5rem;
+    position: relative;
   }
+
   .activate {
     color: #005eb8;
-    border-bottom: 0.2rem solid #005eb8;
+  }
+
+  .activate::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 0.2rem;
+    background-color: #005eb8;
+    animation: borderExpand 0.3s forwards;
+  }
+
+  @keyframes borderExpand {
+    from {
+      width: 0;
+    }
+    to {
+      width: 100%;
+    }
   }
 `;
