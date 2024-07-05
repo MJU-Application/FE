020fc924001b9928607ba1007b8ff232299f0c62
qwer0114 <skyok203@naver.com>
2024-07-05 23:42:45 +0900

    feat: Constant 정의

---
+++ src/constants/notice.ts
@@ -0,0 +1,7 @@
+const NOTICE = [
+  { name: "일반", query: "?type=general" },
+  { name: "학사", query: "?type=school" },
+  { name: "장학/학자금", query: "?type=loan" },
+  { name: "취창업", query: "?type=job" },
+];
+export { NOTICE };
